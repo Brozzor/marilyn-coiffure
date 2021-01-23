ef889bfec695661497426b8d2d556716ec65e15e
bertyn99 <bboulikou@gmail.com>
2021-01-23 18:27:44 +0100

    decomposition du template en composant

---
+++ components/AddBooking.vue
@@ -0,0 +1,10 @@
+<template></template>
+
+<script>
+export default {
+  name: 'AddBooking',
+}
+</script>
+
+<style>
+</style>
---
+++ components/Service.vue
@@ -0,0 +1,164 @@
+<template>
+  <!--Service section -->
+  <section id="services" class="container our-service">
+    <div class="row">
+      <h2 class="heading-title">Our Services</h2>
+      <div class="service-wrapper clearfix">
+        <div class="service-block service-middle">
+          <div class="service-block-inner service-1-hover">
+            <div class="service-block-image">
+              <div class="service-wrap-img">
+                <img src="images/service-img.jpg" alt="" />
+              </div>
+              <div class="service-block-image-wrap">
+                <div class="service-ic">
+                  <img src="images/classic-cut.png" alt="" />
+                </div>
+                <h6>Classic Hair Cut</h6>
+                <p>
+                  Lorem Ipsum is simply<br />
+                  dummy text of the printinorem<br />
+                  Ipsum is simply mmy text of the printing<br />
+                  and typeseing industry Lorem
+                </p>
+                <a href="#" title="read more" class="read-more-btn"
+                  >read more</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="service-block service-middle">
+          <div class="service-block-inner service-2-hover">
+            <div class="service-block-image">
+              <div class="service-wrap-img">
+                <img src="images/service-img.jpg" alt="" />
+              </div>
+              <div class="service-block-image-wrap">
+                <div class="service-ic">
+                  <img src="images/clipper-cut.png" alt="" />
+                </div>
+                <h6>Clipper Cut</h6>
+                <p>
+                  Lorem Ipsum is simply<br />
+                  dummy text of the printinorem<br />
+                  Ipsum is simply mmy text of the printing<br />
+                  and typeseing industry Lorem
+                </p>
+                <a href="#" title="read more" class="read-more-btn"
+                  >read more</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="service-block service-middle">
+          <div class="service-block-inner service-3-hover">
+            <div class="service-block-image">
+              <div class="service-wrap-img">
+                <img src="images/service-img.jpg" alt="" />
+              </div>
+              <div class="service-block-image-wrap">
+                <div class="service-ic">
+                  <img src="images/razoe-shaver.png" alt="" />
+                </div>
+                <h6>Razoe Shaver</h6>
+                <p>
+                  Lorem Ipsum is simply<br />
+                  dummy text of the printinorem<br />
+                  Ipsum is simply mmy text of the printing<br />
+                  and typeseing industry Lorem
+                </p>
+                <a href="#" title="read more" class="read-more-btn"
+                  >read more</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="service-block service-middle">
+          <div class="service-block-inner service-4-hover">
+            <div class="service-block-image">
+              <div class="service-wrap-img">
+                <img src="images/service-img.jpg" alt="" />
+              </div>
+              <div class="service-block-image-wrap">
+                <div class="service-ic">
+                  <img src="images/classic-cut.png" alt="" />
+                </div>
+                <h6>Classic Hair Cut</h6>
+                <p>
+                  Lorem Ipsum is simply<br />
+                  dummy text of the printinorem<br />
+                  Ipsum is simply mmy text of the printing<br />
+                  and typeseing industry Lorem
+                </p>
+                <a href="#" title="read more" class="read-more-btn"
+                  >read more</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="service-block service-middle active">
+          <div class="service-block-inner service-5-hover">
+            <div class="service-block-image">
+              <div class="service-wrap-img">
+                <img src="images/service-img.jpg" alt="" />
+              </div>
+              <div class="service-block-image-wrap">
+                <div class="service-ic">
+                  <img src="images/clipper-cut.png" alt="" />
+                </div>
+                <h6>Clipper Cut</h6>
+                <p>
+                  Lorem Ipsum is simply<br />
+                  dummy text of the printinorem<br />
+                  Ipsum is simply mmy text of the printing<br />
+                  and typeseing industry Lorem
+                </p>
+                <a href="#" title="read more" class="read-more-btn"
+                  >read more</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="service-block service-middle">
+          <div class="service-block-inner service-6-hover">
+            <div class="service-block-image">
+              <div class="service-wrap-img">
+                <img src="images/service-img.jpg" alt="" />
+              </div>
+              <div class="service-block-image-wrap">
+                <div class="service-ic">
+                  <img src="images/razoe-shaver.png" alt="" />
+                </div>
+                <h6>Razoe Shaver</h6>
+                <p>
+                  Lorem Ipsum is simply<br />
+                  dummy text of the printinorem<br />
+                  Ipsum is simply mmy text of the printing<br />
+                  and typeseing industry Lorem
+                </p>
+                <a href="#" title="read more" class="read-more-btn"
+                  >read more</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Service',
+}
+</script>
+
+<style>
+</style>
