7191e6e01a9c7ef29be53b8e93f29f6a981194c7
bertyn99 <bboulikou@gmail.com>
2021-01-23 18:31:07 +0100

    ajout de Testimony

---
+++ components/Testimony.vue
@@ -0,0 +1,74 @@
+<template>
+  <!--Testimonial section -->
+  <section id="testimonial" class="container client-testimonial">
+    <div class="row">
+      <h2 class="heading-title">50k+ Happy Clients</h2>
+      <div class="testimonial-wrapper">
+        <div class="testimonial-main">
+          <div class="testimonial-box">
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typing indus
+              try<br />Lorem Ipsum has een the indus try's stmmy text of the
+              printing and<br />
+              typeseing industry.
+            </p>
+            <span><b>Devil Shohemark,</b>CEO microsoft</span>
+          </div>
+          <div class="test-img">
+            <img src="images/testi-img.png" alt="" />
+          </div>
+        </div>
+        <div class="testimonial-main">
+          <div class="testimonial-box">
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typing indus
+              try<br />Lorem Ipsum has een the induy dummy text of the printing
+              and<br />
+              typeseing industry.
+            </p>
+            <span><b>Devil Shohemark,</b> CEO microsoft</span>
+          </div>
+          <div class="test-img">
+            <img src="images/testi-img.png" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div class="testimonial-logo clearfix">
+        <div class="testi-logo-img">
+          <a href="#" title=""><img src="images/logo1.png" alt="" /></a>
+        </div>
+        <div class="testi-logo-img">
+          <a href="#" title=""><img src="images/logo2.png" alt="" /></a>
+        </div>
+        <div class="testi-logo-img">
+          <a href="#" title=""><img src="images/logo3.png" alt="" /></a>
+        </div>
+        <div class="testi-logo-img">
+          <a href="#" title=""><img src="images/logo4.png" alt="" /></a>
+        </div>
+        <div class="testi-logo-img">
+          <a href="#" title=""><img src="images/logo1.png" alt="" /></a>
+        </div>
+        <div class="testi-logo-img">
+          <a href="#" title=""><img src="images/logo2.png" alt="" /></a>
+        </div>
+        <div class="testi-logo-img">
+          <a href="#" title=""><img src="images/logo3.png" alt="" /></a>
+        </div>
+        <div class="testi-logo-img">
+          <a href="#" title=""><img src="images/logo4.png" alt="" /></a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Testimony',
+}
+</script>
+
+<style>
+</style>
