a5c0baacaa15af03450898827002b023ac74bdd1
bertyn99 <bboulikou@gmail.com>
2021-01-23 19:05:34 +0100

    modif temoinage

--- components/Testimony.vue
+++ components/Testimony.vue
@@ -18,35 +18,9 @@
             <img src="images/testi-img.png" alt="" />
           </div>
         </div>
-        <div class="testimonial-main">
-          <div class="testimonial-box">
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typing indus
-              try<br />Lorem Ipsum has een the induy dummy text of the printing
-              and<br />
-              typeseing industry.
-            </p>
-            <span><b>Devil Shohemark,</b> CEO microsoft</span>
-          </div>
-          <div class="test-img">
-            <img src="images/testi-img.png" alt="" />
-          </div>
-        </div>
       </div>
 
       <div class="testimonial-logo clearfix">
-        <div class="testi-logo-img">
-          <a href="#" title=""><img src="images/logo1.png" alt="" /></a>
-        </div>
-        <div class="testi-logo-img">
-          <a href="#" title=""><img src="images/logo2.png" alt="" /></a>
-        </div>
-        <div class="testi-logo-img">
-          <a href="#" title=""><img src="images/logo3.png" alt="" /></a>
-        </div>
-        <div class="testi-logo-img">
-          <a href="#" title=""><img src="images/logo4.png" alt="" /></a>
-        </div>
         <div class="testi-logo-img">
           <a href="#" title=""><img src="images/logo1.png" alt="" /></a>
         </div>
