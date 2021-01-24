968ee49b7b36bb7611a610a1fbf0d61557500d1e
Brozzor <46285168+Brozzor@users.noreply.github.com>
2021-01-24 16:28:18 +0100

    création du composant Gallery et Pricing

---
+++ components/Pricing.vue
@@ -0,0 +1,175 @@
+<template>
+    <section id="pricing" class="container our-pricing">
+      <div class="row">
+        <h2 class="heading-title">Our Pricing</h2>
+        <div class="our-pricing-wrapper clearfix">
+          <div class="our-price-box">
+            <div class="our-price-box-inner clearfix">
+              <div class="our-price-box-left">
+                <div class="our-price-ic">
+                  <img src="images/price-ic1.png" alt="" />
+                </div>
+                <div class="our-price-text">
+                  <h4>Classic Hair Cut</h4>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printorem Ipsum is
+                    simply mmy text of the printing and typing.
+                  </p>
+                  <a href="#" title="more" class="more">more</a>
+                </div>
+              </div>
+              <div class="our-price-box-right">
+                <div class="price-discount">
+                  <span>25% off</span>
+                </div>
+                <div class="start-price">
+                  <span>start from</span>
+                  <b>$ 12</b>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="our-price-box">
+            <div class="our-price-box-inner clearfix">
+              <div class="our-price-box-left">
+                <div class="our-price-ic">
+                  <img src="images/price-ic2.png" alt="" />
+                </div>
+                <div class="our-price-text">
+                  <h4>Razoe Shaver</h4>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printorem Ipsum is
+                    simply mmy text of the printing and typing.
+                  </p>
+                  <a href="#" title="more" class="more">more</a>
+                </div>
+              </div>
+              <div class="our-price-box-right">
+                <div class="table">
+                  <div class="table-cell">
+                    <div class="start-price">
+                      <span>start from</span>
+                      <b>$ 12</b>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="our-price-box">
+            <div class="our-price-box-inner clearfix">
+              <div class="our-price-box-left">
+                <div class="our-price-ic">
+                  <img src="images/price-ic3.png" alt="" />
+                </div>
+                <div class="our-price-text">
+                  <h4>Combing</h4>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printorem Ipsum is
+                    simply mmy text of the printing and typing.
+                  </p>
+                  <a href="#" title="more" class="more">more</a>
+                </div>
+              </div>
+              <div class="our-price-box-right">
+                <div class="table">
+                  <div class="table-cell">
+                    <div class="start-price">
+                      <span>start from</span>
+                      <b>$ 12</b>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="our-price-box">
+            <div class="our-price-box-inner clearfix">
+              <div class="our-price-box-left">
+                <div class="our-price-ic">
+                  <img src="images/price-ic4.png" alt="" />
+                </div>
+                <div class="our-price-text">
+                  <h4>Mackup</h4>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printorem Ipsum is
+                    simply mmy text of the printing and typing.
+                  </p>
+                  <a href="#" title="more" class="more">more</a>
+                </div>
+              </div>
+              <div class="our-price-box-right">
+                <div class="price-discount">
+                  <span>25% off</span>
+                </div>
+                <div class="start-price">
+                  <span>start from</span>
+                  <b>$ 12</b>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="our-price-box">
+            <div class="our-price-box-inner clearfix">
+              <div class="our-price-box-left">
+                <div class="our-price-ic">
+                  <img src="images/price-ic5.png" alt="" />
+                </div>
+                <div class="our-price-text">
+                  <h4>Clipper Cut</h4>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printorem Ipsum is
+                    simply mmy text of the printing and typing.
+                  </p>
+                  <a href="#" title="more" class="more">more</a>
+                </div>
+              </div>
+              <div class="our-price-box-right">
+                <div class="table">
+                  <div class="table-cell">
+                    <div class="start-price">
+                      <span>start from</span>
+                      <b>$ 12</b>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="our-price-box">
+            <div class="our-price-box-inner clearfix">
+              <div class="our-price-box-left">
+                <div class="our-price-ic">
+                  <img src="images/price-ic6.png" alt="" />
+                </div>
+                <div class="our-price-text">
+                  <h4>Mustache</h4>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printorem Ipsum is
+                    simply mmy text of the printing and typing.
+                  </p>
+                  <a href="#" title="more" class="more">more</a>
+                </div>
+              </div>
+              <div class="our-price-box-right">
+                <div class="table">
+                  <div class="table-cell">
+                    <div class="start-price">
+                      <span>start from</span>
+                      <b>$ 12</b>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'Pricing',
+}
+</script>
