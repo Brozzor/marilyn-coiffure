f7c09433d6a822ada1cca598377bf7bc01b0c2df
bertyn99 <bboulikou@gmail.com>
2021-01-24 15:56:32 +0100

    ajout du store

---
+++ store/user.js
@@ -0,0 +1,53 @@
+import Api from 'axios'
+export const state = () => ({
+    currentUser: {}
+})
+
+export const mutations = {
+    SET_CURRENT_TOKEN(state, token) {
+        localStorage.tokenSession = token
+
+    },
+
+
+}
+
+export const action = {
+    async register({ commit }, form) {
+        try {
+            const response = await Api.post(
+                localStorage.server + '/register',
+                form
+            ).catch(err => {
+                if (err.response.status === 400) {
+                    throw new Error(err.response.data.status)
+                }
+                throw err
+            })
+            const token = response.headers.tokensession
+            commit('SET_CURRENT_TOKEN', token)
+            return true
+        } catch (error) {
+            console.log(error)
+            return { error: error.message }
+        }
+    },
+    async signIn({ commit }, form) {
+        try {
+            const response = await Api.post(
+                localStorage.server + '/connect',
+                form
+            ).catch(err => {
+                if (err.response.status === 400) {
+                    throw new Error(err.response.data.status)
+                }
+                throw err
+            })
+            const token = response.headers.tokensession
+            commit('SET_CURRENT_TOKEN', token)
+            return true
+        } catch (error) {
+            return { error: error.message }
+        }
+    },
+}
