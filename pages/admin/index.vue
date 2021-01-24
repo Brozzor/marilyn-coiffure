801e3533697b200fa11d209c82bb53365b46ad10
bertyn99 <bboulikou@gmail.com>
2021-01-24 17:28:55 +0100

    changer la version de boostrap

---
+++ pages/admin/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <form class="col-xs-12 col-md-6 mx-auto">
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            placeholder="Email"
+          />
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+        </div>
+
+        <button type="submit" class="btn btn-default">Connexion</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
