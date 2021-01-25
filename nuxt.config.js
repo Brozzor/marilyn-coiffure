defdf4d704bf261509c26738bb48b00df5ba6edf
bertyn99 <bboulikou@gmail.com>
2021-01-25 01:21:02 +0100

    modification url serveur api

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,7 +53,7 @@ export default {
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {
-    baseURL: 'http://localhost:8090'
+    baseURL: 'https://marilyn-coiffure-back.herokuapp.com/'
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
