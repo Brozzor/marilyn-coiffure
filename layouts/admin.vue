21e273b72418ac38c8e4e4a7948d691a29534483
bertyn <bertynboulikou@gmail.com>
2021-01-26 23:31:54 +0100

    ajout des  autres pages admin

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,5 +1,17 @@
 <template>
-  <Nuxt />
+  <div class="leading-normal tracking-normal" id="main-body">
+    <div class="flex flex-wrap">
+      <Sidenav></Sidenav>
+      <div
+        class="w-full bg-gray-100 pl-0 lg:pl-64 min-h-screen"
+        :class="sideBarOpen ? 'overlay' : ''"
+        id="main-content"
+      >
+        <div class="p-6 bg-gray-100 mb-20"><Nuxt /></div>
+        <Footer></Footer>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
--- pages/admin/dashboard.vue
+++ pages/admin/dashboard.vue
@@ -1,17 +1,17 @@
 <template>
-  <div>
-    <Sidenav></Sidenav>
-
-    <Footer></Footer>
-  </div>
+  <div></div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import Sidenav from '~/components/admin/Sidenav.vue'
 import FooterAdmin from '~/components/admin/FooterAdmin.vue'
 export default {
   components: { Sidenav, FooterAdmin },
   layout: 'admin',
+  computed: {
+    ...mapState(['sideBarOpen']),
+  },
 }
 </script>
 
