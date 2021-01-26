42042dad8a11664e087eeaf755bca83c7f9e23bc
bertyn <bertynboulikou@gmail.com>
2021-01-26 21:18:27 +0100

    correction css layout admin

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -5,7 +5,16 @@
 </template>
 
 <script>
-export default {}
+export default {
+  head() {
+    return {
+      link: [
+        { rel: 'stylesheet', href: '/css/argon.css' },
+        { rel: 'stylesheet', href: '/css/nucleo.css' },
+      ],
+    }
+  },
+}
 </script>
 
 <style>
