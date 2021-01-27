c1e94c40e30488d00e458c76354011bc79c21c06
Brozzor <46285168+Brozzor@users.noreply.github.com>
2021-01-27 19:30:15 +0100

    création de la page reservation dans le panel admin

--- store/admin.js
+++ store/admin.js
@@ -23,4 +23,24 @@ export const actions = {
       return { error: error.message }
     }
   },
+  async reservation({ commit }) {
+
+    try {
+      const response = await this.$axios.$get('/admin/reservation', {
+          headers: {
+            tokenSession: localStorage.tokenSession,
+          }
+        })
+        .catch((err) => {
+          if (err.response.status === 400) {
+            throw new Error(err.response.data.error)
+          }
+          throw err
+        })
+      return response
+    } catch (error) {
+      console.log(error)
+      return { error: error.message }
+    }
+  },
 }
