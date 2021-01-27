bb6bdddb5f610e2381d73c15046074717be1433b
bertyn <bertynboulikou@gmail.com>
2021-01-27 01:54:27 +0100

    add lastbooking

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  purge: [
-    './components/**/*.{vue,js}',
-    './layouts/**/*.vue',
-    './pages/admin/*.vue',
-    './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
-  ],
+  purge: {
+    preserveHtmlElements: false, content: [
+      './components/**/*.{vue,js}',
+      './layouts/**/*.vue',
+      './pages/**/*.vue',
+      './plugins/**/*.{js,ts}',
+      './nuxt.config.{js,ts}',
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
